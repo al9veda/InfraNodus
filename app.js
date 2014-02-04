158c46dff996f00d6210f1a943dc359a5472c597
deemeetree <info@noduslabs.com>
2014-02-04 15:02:16 +0100

    added basic handidling of registration and error messages for user

--- app.js
+++ app.js
@@ -5,9 +5,19 @@
 
 var express = require('express');
 var routes = require('./routes');
+
+// add a route for user processing
 var user = require('./routes/user');
+
+// add a route for registration (the destination for users when they request it)
+var register = require('./routes/register');
+
+// add a business logic for displaying messages (not a route as we don't need the user to access them directly)
+var messages = require('./lib/messages');
+
 var http = require('http');
 var path = require('path');
+
 
 var app = express();
 
@@ -15,13 +25,20 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
+
+// made to manage registration process and messages to user
 app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
+app.use(messages);
+
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -32,6 +49,12 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+
+// on get request for /register request a form route function from register.js
+app.get('/register', register.form);
+
+// on post request to /register request a submit rout function from register.js
+//app.post('/register', register.submit);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
