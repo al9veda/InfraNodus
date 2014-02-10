169eb45599442dfbf65e9d858bbdf9cf2c3c8740
deemeetree <info@noduslabs.com>
2014-02-10 14:51:08 +0100

    added plans for work

--- sandbox.js
+++ sandbox.js
@@ -17,8 +17,46 @@ console.log(hashtags);
 
 
 
-// TODO create an internal system of IDs for each node
+/* TODO create an internal system of IDs for every node because then it would add new relaitons between the same nodes just because the creator/context/statement is different - that's what we need.
+   This will probably be done by first querying the context and statement to see if they exist.
+   If yes, we create another transaction where all the references to users, statements, and contexts are done through their IDs
+   Later the same thing might be necessary for nodes as well, but for now we can leave it as it is, before phrases are supported.
 
+
+   TODO Practically - Data Structure
+   A user should have an ID generated first and it's carried into the session variable with him (not his internal ID)
+   If we work with a context, it has been already created, so it already has an ID. If not, then generate an ID for it.
+   If we work with a statement, it's always new, so a new ID is generated for it.
+
+   The MERGE statement will always ensure that if we come in with some new data that HAS to be recorded, a new node / edge will be added into the system
+
+   The nodes should probably also have a unique ID, but we can always add this in later as in this point we do not need that
+
+   TODO After this above is done
+   Implement this logic into the interface
+
+   TODO After the interface is done
+   Check the imput (statement and hashtags) for injections etc.
+
+   TODO Formalize this logic into a post on Nodus Labs and for KnowNodes
+
+   TODO After inputs are checked
+   Add another login system, check MongoDB or mySQL, maybe MongoDB is better to support large document storage
+
+   TODO Phrase support
+   Add support for phrase tags
+
+   TODO API
+   Add all this functionality to APIs
+
+   TODO Viz
+   Visualize the graphs through Sigma
+
+   TODO Interface
+   Create better way to create contexts, etc. etc.
+
+
+*/
 
 // Construct CREATE Cypher query
 
