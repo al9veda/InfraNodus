491e10725bccf0ebad39c7f654aebc2427dbf027
deemeetree <info@noduslabs.com>
2014-03-10 19:50:26 +0100

    some basics added, broken

--- app.js
+++ app.js
@@ -38,13 +38,82 @@ var messages = require('./lib/messages');
 var http = require('http');
 var path = require('path');
 var leap = require('./routes/leap');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+var users = [
+    {id: 1, username: 'admin', name: 'admin', password: 'tutu', neo_id: '0', neo_uid: 'b9745f70-a13f-11e3-98c5-476729c16049', email: 'info@noduslabs.com' },
+    {id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
+]
+
+function findById(id, fn) {
+    var idx = id - 1;
+    if (users[idx]) {
+        fn(null, users[idx]);
+    } else {
+        fn(new Error('User ' + id + ' does not exist'));
+    }
+}
+
+function findByUsername(username, fn) {
+    for (var i = 0, len = users.length; i < len; i++) {
+        var user = users[i];
+        if (user.username === username) {
+            return fn(null, user);
+        }
+    }
+    return fn(null, null);
+}
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+//   Use the LocalStrategy within Passport.
+//   Strategies in passport require a `verify` function, which accept
+//   credentials (in this case, a username and password), and invoke a callback
+//   with a user object.  In the real world, this would query a database;
+//   however, in this example we are using a baked-in set of users.
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        // asynchronous verification, for effect...
+        process.nextTick(function () {
+
+            // Find the user by username.  If there is no user with the given
+            // username, or the password is not correct, set the user to `false` to
+            // indicate failure and set a flash message.  Otherwise, return the
+            // authenticated `user`.
+            findByUsername(username, function(err, user) {
+                if (err) { return done(err); }
+                if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+                if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
+                return done(null, user);
+            })
+        });
+    }
+));
 
 var app = express();
+
+
+
 
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
 
 
 app.use(express.bodyParser());
@@ -55,12 +124,13 @@ app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 // This makes sure that when someone accesses /api they are authenticated first
 //app.use('/api', api.auth);
 app.use(user);
 app.use(messages);
-app.use(app.router);
 app.use(app.router);
 app.use(routes.notfound);
 app.use(routes.error);
@@ -74,7 +144,26 @@ if ('development' == app.get('env')) {
 app.get('/register', register.form);
 app.post('/register', register.submit);
 app.get('/login', login.form);
-app.post('/login', login.submit);
+
+// POST /login
+//   This is an alternative implementation that uses a custom callback to
+//   acheive the same functionality.
+app.post('/login', function(req, res, next) {
+    passport.authenticate('local', function(err, user, info) {
+        if (err) { return next(err) }
+        if (!user) {
+            req.session.messages =  [info.message];
+            return res.redirect('/login')
+        }
+        req.logIn(user, function(err) {
+            if (err) { return next(err); }
+            console.log(user);
+            return res.redirect('/');
+
+        });
+    })(req, res, next);
+});
+
 app.get('/logout', login.logout);
 app.get('/post', entries.form);
 app.post(
@@ -113,3 +202,13 @@ if (process.env.ERROR_ROUTE) {
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) { return next(); }
+    res.redirect('/login')
+}
--- routes/entries.js
+++ routes/entries.js
@@ -26,6 +26,7 @@ exports.list = function(req, res, next){
 
     var perceivername = null;
 
+    console.log('user: ' + res.locals.user);
     // Is the user logged in? Then he is the receiver
     if (res.locals.user) {
         receiver = res.locals.user.neo_uid;
