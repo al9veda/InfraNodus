bdf71f2efabe0e401cc2e5fbfcb0c9b5a4db5961
deemeetree <info@noduslabs.com>
2015-04-12 17:11:35 +0200

    basic form submission socket logic

--- app.js
+++ app.js
@@ -170,6 +170,13 @@ appio.listen(app.get('port'), function(){
 
 io.on('connection', function(socket){
     console.log('a user socket connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
 });
 
 
