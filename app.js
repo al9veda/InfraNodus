85903d42e417e2e6cc6f79a82f6c71b14b0cb733
deemeetree <info@noduslabs.com>
2015-04-20 19:01:48 +0200

    now deletes statement and removes it from entries

--- app.js
+++ app.js
@@ -178,6 +178,11 @@ var chat = io.on('connection', function(socket){
         io.sockets.in(socket.room).emit('chat message', msg);
         // io.emit('chat message', msg);
     });
+    socket.on('delete message', function(msg){
+        console.log('message: ' + msg);
+        io.sockets.in(socket.room).emit('delete message', msg);
+        // io.emit('chat message', msg);
+    });
     socket.on('node click', function(msg){
         socket.broadcast.to(socket.room).emit('node click', msg);
     });
