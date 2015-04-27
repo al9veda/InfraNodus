3c72515751958fcca312bcc199c381fb5f81e04c
deemeetree <info@noduslabs.com>
2015-04-27 03:08:36 +0200

    nice scroll on iphone

--- public/javascripts/jquery.fullPage.js
+++ public/javascripts/jquery.fullPage.js
@@ -1504,15 +1504,15 @@
                 var isResponsive = container.hasClass(RESPONSIVE);
                 if ($window.width() < options.responsive ){
                     if(!isResponsive){
-                        FP.setAutoScrolling(false, 'internal');
+                        /*FP.setAutoScrolling(false, 'internal');*/
                         FP.setFitToSection(false, 'internal');
-                        $(SECTION_NAV_SEL).hide();
+                        /*$(SECTION_NAV_SEL).hide();*/
                         container.addClass(RESPONSIVE);
                     }
                 }else if(isResponsive){
-                    FP.setAutoScrolling(originals.autoScrolling, 'internal');
+                    /*FP.setAutoScrolling(originals.autoScrolling, 'internal');*/
                     FP.setFitToSection(originals.autoScrolling, 'internal');
-                    $(SECTION_NAV_SEL).show();
+                   /* $(SECTION_NAV_SEL).show();*/
                     container.removeClass(RESPONSIVE);
                 }
             }
